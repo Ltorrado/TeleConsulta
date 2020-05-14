8f0ad64dbc52ed2d5717d77ce5ee87bbce35ddf8
Leonardo Torrado <ltorrado@DOMAIN.LOCAL>
2020-05-14 16:00:00 -0500

    chat con imagenes

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,36 +2,98 @@
 
 (function(){
 
+   scrollAbajo= function(ok){
+     debugger
+
+    chat.scrollToBottom();
+  }
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+
+  var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  $("#myModal").modal('hide')
+}
+  $('body').on('click','img',function(){
+    debugger
+    $("#myModal").modal();
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  })
+
+$('img').on("click",  function(){
+  debugger
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+})
+
+$('body').on('load','img',function(){
+  debugger
+  $(this).css('background','none');
+  console.log('imagenCargada')
+  chat.scrollToBottom();
+});
+
 
   var intials = this.loadingParams.nombreRemisor.charAt(0);
  $('#profileImage').text(intials);
   
-    var ural="https://appcita.viva1a.com.co:8051/teleconsulta/";
-  //var ural ="http://localhost:56508/"
+   // var ural="https://appcita.viva1a.com.co:8051/teleconsulta/";
+  var ural ="http://localhost:56508/teleconsulta/"
 
   var   connection = new signalR.HubConnectionBuilder().withUrl(ural+"chatHub")
   .configureLogging(signalR.LogLevel.Information)
   .build();
   var chat = {
-     mensajeTemplate: ' <li class="clearfix">'+
-'<div class="message-data align-right">'+
- ' <span class="message-data-time" >{{time}}, Hoy</span> &nbsp; &nbsp;'+
-  '<span class="message-data-name" ></span> <i class="fa fa-circle me"></i>'+
-'</div>'+
-'<div class="message other-message float-right">'+
-  '{{messageOutput}}'+
-'</div>'+
-'</li>',
+    isMensajeConImage:false,
+    llevaImagen:function(){
+      debugger
+        if(this.isMensajeConImage){
+          return '<div style="overflow:hidden" >'+
+          '<img  style="zoom:40%" onload="scrollAbajo(this);" src={{imagen}}>'
+          +'</div>'
 
- respuestaTemplate : '  <li>'+
+
+
+        }else{
+          return ''
+        }
+        return'';
+    
+    },
+     mensajeTemplate:function(){
+       return  ' <li class="clearfix">'+
+       '<div class="message-data align-right">'+
+        ' <span class="message-data-time" >{{time}}, Hoy</span> &nbsp; &nbsp;'+
+         '<span class="message-data-name" ></span> <i class="fa fa-circle me"></i>'+
+       '</div>'+
+       
+       '<div class="message other-message float-right">'+
+       
+       this.llevaImagen()  
+       +
+         '{{messageOutput}}'+
+       '</div>'+
+       '</li>'
+     
+     } ,
+
+ respuestaTemplate : function(){return '  <li>'+
 '<div class="message-data">'+
   '<span class="message-data-name"><i class="fa fa-circle online"></i> </span>'+
   '<span class="message-data-time">{{time}}, Hoy</span>'+
 '</div>'+
-'<div class="message my-message">'+
+'<div class="message my-message">'+       
+       this.llevaImagen()  
+       +
   '{{response}}'+
 '</div>'+
-'</li>',
+'</li>'},
 
     messageToSend: '',
   
@@ -51,23 +113,31 @@
     },
     recivirRespuestaEvento : function(){
 
-      connection.on("NuevoMensaje", (user, message) => {
+      connection.on("NuevoMensaje", (user, datos) => {
         debugger
+            
               if(loadingParams.clientid == user){
                 this.scrollToBottom();
-                if (message.trim() !== '') {
+                if (datos.mensaje.trim() !== '') {
                   debugger
-                  var template = Handlebars.compile(this.respuestaTemplate);
+                  if(datos.imagen.trim() !== '' ){
+                    this.isMensajeConImage=true;
+                  }
+                  var html=this.respuestaTemplate()
+                  var template = Handlebars.compile(html);
 
                   var context = { 
-                    response: message,
-                    time: this.getCurrentTime()
+                    response: datos.mensaje,
+                    time: this.getCurrentTime(),
+                    imagen:datos.imagen
                   };
                   time=this.getCurrentTime();
 
                   this.$chatHistoryList.append(template(context));
+                  
+                  this.$textarea.val(''); 
+                  this.isMensajeConImage=false;      
                   this.scrollToBottom();
-                  this.$textarea.val('');       
               
                 } 
               }
@@ -98,36 +168,70 @@
       this.scrollToBottom();
       if (this.messageToSend.trim() !== '') {
         debugger
-        var template = Handlebars.compile(this.mensajeTemplate);
+      
 
-        var context = { 
-          messageOutput: this.messageToSend,
-          time: this.getCurrentTime()
-        };
-        time=this.getCurrentTime();
+        input=document.getElementById("selectedFile");
+             
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+  
+            reader.onload = this.imagenCargada;
+  
+            reader.readAsDataURL(input.files[0]);
+        }else{
+          var mensaje=this.mensajeTemplate();
+          var template = Handlebars.compile(this.mensajeTemplate());
+          var context = { 
+            messageOutput: this.messageToSend,
+            time: this.getCurrentTime()
+          };
+          time=this.getCurrentTime();
+  
+          this.$chatHistoryList.append(template(context));
+          this.EnivarMensajeServidor( this.messageToSend.trim());
+          this.scrollToBottom();
+          this.$textarea.val('');       
+          
 
-        this.$chatHistoryList.append(template(context));
-        this.EnivarMensajeServidor( this.messageToSend.trim());
-        this.scrollToBottom();
-        this.$textarea.val('');       
-        
+
+        }
       }
       
     },    
+    imagenCargada:function (e) {
+      debugger
+      chat.isMensajeConImage=true;   
+      var template = Handlebars.compile(chat.mensajeTemplate());
+      var context = { 
+        messageOutput: chat.messageToSend,
+        time: chat.getCurrentTime(),
+         imagen: e.target.result
+      };  
+ 
+      chat.$chatHistoryList.append(template(context));
+      chat.EnivarMensajeServidor( chat.messageToSend.trim());
+     
+      chat.isMensajeConImage=false;   
+      chat.$textarea.val('');       
+    },
     addMessage: function() {
       this.messageToSend = this.$textarea.val()
       this.render();         
     },
     addimagen: function(input) {
       debugger;
+      input=document.getElementById("selectedFile");
+      preview=$("#imagentumb");
+      chatHistory=$(".chat-history")
+      
       if (input.files && input.files[0]) {
           var reader = new FileReader();
 
           reader.onload = function (e) {
-              $('#blah')
-                  .attr('src', e.target.result)
-                  .width(150)
-                  .height(200);
+
+            preview.attr('src', e.target.result);;
+            preview.show();
+            chatHistory.hide()
           };
 
           reader.readAsDataURL(input.files[0]);
@@ -140,20 +244,34 @@
         }
     },
     EnivarMensajeServidor:function(mensaje){
+      debugger
+
+      input=document.getElementById("selectedFile");
+      preview=$("#imagentumb");
+      chatHistory=$(".chat-history")
+      var formData = new FormData();
+formData.append("imagen", document.getElementById("selectedFile").files[0]);
+formData.append("mensaje", mensaje);
+formData.append("usuarioId", loadingParams.clientid);
+formData.append('tipo',loadingParams.tipo);
+formData.append('usuarioReceptor',loadingParams.remisorChat);
+formData.append('citaId',loadingParams.roomId);
+
       $.ajax({
 
         url : ural+'api/Chat/GuardarMensaje',
-        type : 'GET',
-        data : {
-            'mensaje' : mensaje,
-            'usuarioId':loadingParams.clientid,
-            'usuarioReceptor':loadingParams.remisorChat,
-            '':loadingParams.tipo,
-            'citaId':loadingParams.roomId,
-        },
-        dataType:'json',
+        type : 'POST',
+        data :formData,  
+         processData: false,
+        contentType: false,
+      
+        enctype: 'multipart/form-data',
         success : function(data) {              
-         
+          preview.attr('src', null);;
+          input.value = "";
+          preview.hide();
+          chatHistory.show()
+          chat.scrollToBottom();
         },
         error : function(request,error)
         {
